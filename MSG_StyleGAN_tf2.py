Fixed bug in the moving average. Gradients from instaneous rather than average. Cleanup utilities

MSG_StyleGAN_tf2.py
@@ -68,15 +68,16 @@ def make_mapping_model():
     # Update moving average of W.
     if dlatent_avg_beta is not None:
         batch_avg = tf.reduce_mean(dlatents[:, 0], axis=0)
-        update_op = dlatent_avg, batch_avg - (dlatent_avg - batch_avg)*dlatent_avg_beta
+        update_op = tf.compat.v1.assign(dlatent_avg, lerp(batch_avg, dlatent_avg, dlatent_avg_beta))
         with tf.control_dependencies([update_op]):
             dlatents = tf.identity(dlatents)
 
-    if truncation_psi is not None and truncation_cutoff is not None:
-        layer_idx = np.arange(G_LAYERS)[np.newaxis, :, np.newaxis]
-        ones = np.ones(layer_idx.shape, dtype=DTYPE)
-        coefs = tf.cast(tf.where(layer_idx < truncation_cutoff, truncation_psi * ones, ones), DTYPE)
-        dlatents = dlatent_avg + (dlatents - dlatent_avg) * coefs
+    if (not TRAIN):
+        if truncation_psi is not None and truncation_cutoff is not None:
+            layer_idx = np.arange(G_LAYERS)[np.newaxis, :, np.newaxis]
+            ones = np.ones(layer_idx.shape, dtype=DTYPE)
+            coefs = tf.cast(tf.where(layer_idx < truncation_cutoff, truncation_psi * ones, ones), DTYPE)
+            dlatents = dlatent_avg + (dlatents - dlatent_avg) * coefs
 
 
     mapping_model     = Model(inputs=latents_in, outputs=dlatents)
@@ -92,7 +93,7 @@ def make_synthesis_model():
     use_pixel_norm    = False        # Disable pixelwise feature vector normalization
     use_wscale        = True         # Enable equalized learning rate
     use_instance_norm = True         # Enable instance normalization
-    use_noise         = True         # Enable noise inputs
+    use_noise         = False         # Enable noise inputs
     randomize_noise   = False         # True = randomize noise inputs every time (non-deterministic),
                                      # False = read noise inputs from variables.
     use_styles        = True         # Enable style inputs                             
@@ -314,8 +315,20 @@ def make_discriminator_model():
 
 
 #-------------------------------------define optimizer and loss functions
-generator_optimizer     = tf.keras.optimizers.Adam(learning_rate=GEN_LR, beta_1=0.0, beta_2=0.99, epsilon=1e-8)
-discriminator_optimizer = tf.keras.optimizers.Adam(learning_rate=DIS_LR, beta_1=0.0, beta_2=0.99, epsilon=1e-8)
+lr_gen_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
+    initial_learning_rate=GEN_LR,
+    decay_steps=DECAY_STEPS,
+    decay_rate=DECAY_RATE,
+    staircase=STAIRCASE)
+
+lr_dis_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
+    initial_learning_rate=DIS_LR,
+    decay_steps=DECAY_STEPS,
+    decay_rate=DECAY_RATE,
+    staircase=STAIRCASE)
+
+generator_optimizer     = tf.keras.optimizers.Adam(learning_rate=lr_gen_schedule, beta_1=0.0, beta_2=0.99, epsilon=1e-8)
+discriminator_optimizer = tf.keras.optimizers.Adam(learning_rate=lr_dis_schedule, beta_1=0.0, beta_2=0.99, epsilon=1e-8)
 
 
 #-------------------------------------create an instance of the generator and discriminator

functions.py
@@ -15,6 +15,13 @@ import matplotlib.pyplot as plt
 
 from parameters import *
 from tensorflow.keras import layers
+from typing import Union
+
+#A type that represents a valid Tensorflow expression
+TfExpression = Union[tf.Tensor, tf.Variable, tf.Operation]
+
+#A type that can be converted to a valid Tensorflow expression
+TfExpressionEx = Union[TfExpression, int, float, np.ndarray]
 
 
 #---------------------functions to build StyleGAN network--------------------
@@ -53,6 +60,11 @@ def periodic_padding_flexible(tensor, axis, padding=1):
 
     return tensor
 
+#-------------linear interpolation
+def lerp(a: TfExpressionEx, b: TfExpressionEx, t: TfExpressionEx) -> TfExpressionEx:
+    with tf.name_scope("Lerp"):
+        return a + (b - a) * t
+    
 
 #-------------Pixel normalization
 def pixel_norm(x, epsilon=1e-8):
@@ -497,9 +509,11 @@ def generate_and_save_images(mapping_ave, synthetic_ave, input, iteration):
         pow2 = 2**(res+2)
         nr = np.int(np.sqrt(NEXAMPLES))
         nc = np.int(NEXAMPLES/nr)
-        ninc = 10*nc/4
-        ninr = 10*nr/4
-        fig, axs = plt.subplots(nr,nc, figsize=(ninc, ninr), dpi=96, squeeze=False)
+        # to maintain a fixed figure size
+        # ninc = 10*nc/4
+        # ninr = 10*nr/4
+        # fig, axs = plt.subplots(nr,nc, figsize=(ninc, ninr), dpi=96, squeeze=False)
+        fig, axs = plt.subplots(nr,nc, squeeze=False)
         plt.subplots_adjust(wspace=0.01, hspace=0.01)
         axs = axs.ravel()
         for i in range(NEXAMPLES):
@@ -530,7 +544,7 @@ def generate_and_save_images(mapping_ave, synthetic_ave, input, iteration):
             else:
                 axs[i].imshow(img)
 
-        fig.savefig('images/image_{:d}x{:d}/it_{:06d}.png'.format(pow2,pow2,iteration))
+        fig.savefig('images/image_{:d}x{:d}/it_{:06d}.png'.format(pow2,pow2,iteration), bbox_inches='tight', pad_inches=0)
         plt.close('all')
 
     return div

parameters.py
@@ -13,14 +13,19 @@ import numpy as np
 import os
 
 
-# Mode
-TRAIN      = True
-
-
-# Precision
+# General parameters
 DTYPE = "float32"        # Data type to use for activations and outputs.
 if (DTYPE=="float64"):
     tf.keras.backend.set_floatx('float64')
+TRAIN             = True
+DATASET           = './testloop/data/CASE_NAME/'
+CHKP_DIR          = './checkpoints/'
+CHKP_PREFIX       = os.path.join(CHKP_DIR, 'ckpt')
+PROFILE           = False
+CONVERTTOTFRECORD = False
+USE_GPU           = True
+AUTOTUNE          = tf.data.experimental.AUTOTUNE
+NEXAMPLES         = 1
 
 
 # Network hyper-parameters
@@ -37,6 +42,7 @@ NUM_CHANNELS      = 3                # Number of input color channels. Overridde
 G_LAYERS          = RES_LOG2* 2 - 2  # Numer of layers  
 SCALING_UP        = tf.math.exp( tf.cast(64.0, DTYPE) * tf.cast(tf.math.log(2.0), DTYPE))
 SCALING_DOWN      = tf.math.exp(-tf.cast(64.0, DTYPE) * tf.cast(tf.math.log(2.0), DTYPE))
+R1_GAMMA          = 10  # Gradient penalty coefficient
 
 
 # Training hyper-parameters
@@ -47,10 +53,11 @@ SAVE_EVERY     = 100000
 REDUCE_EVERY   = 100000
 BATCH_SIZE     = 1
 IRESTART       = False
-R1_GAMMA       = 10  # Gradient penalty coefficient
-DATASET        = './testloop/data/CASE/'
-CHKP_DIR       = './checkpoints/'
-CHKP_PREFIX    = os.path.join(CHKP_DIR, 'ckpt')
+GEN_LR         = 3.0e-3
+DIS_LR         = 3.0e-3
+DECAY_STEPS    = TOT_ITERATIONS/5
+DECAY_RATE     = CASE_DECAY_RATE
+STAIRCASE      = True
 G_SMOOTH       = 10.0
 if G_SMOOTH > 0.0:
     Gs_beta = 0.5**tf.math.divide(tf.cast(BATCH_SIZE, DTYPE), G_SMOOTH * 1000.0)

testloop/compare.py
@@ -79,7 +79,7 @@ def compare_images(imgA, imgB, title):
     plt.clim(minDiff,maxDiff)
     plt.suptitle("Statistical differences MSE: %.4f, SSIM: %.4f" % (m, s))
 
-    fig.savefig(title)
+    fig.savefig(title, bbox_inches='tight', pad_inches=0)
 
 
 def trim(im):
@@ -93,7 +93,7 @@ def trim(im):
 
 
 # load the images -- the orig, the orig + fake
-orig = Image.open("./../testloop/data/CASE/test_01.png")
+orig = Image.open("./../testloop/data/CASE_NAME/test_01.png")
 
 # convert to black and white
 orig = orig.convert("L") 
@@ -110,7 +110,7 @@ for i in range(NIMG, 0, -1):
     filename = "./../images/image_{:d}x{:d}/it_{:06d}.png".format(OUTPUT_DIM, OUTPUT_DIM, val)
     temp = Image.open(filename)
     temp = temp.convert("L")
-    temp = trim(temp)
+    #temp = trim(temp)
     temp = temp.resize((OUTPUT_DIM,OUTPUT_DIM))
     atemp = np.asarray(temp, dtype=np.float32)    
     if (i==NIMG):
@@ -124,4 +124,4 @@ if (NIMG>0):
     fake = fake.convert("L") 
 
 # compare the images
-compare_images(orig, fake, "./../testloop/result_CASE.png")
+compare_images(orig, fake, "./../testloop/result_CASE_NAME.png")

train.py
@@ -30,8 +30,8 @@ from tensorflow.keras.models import Model
 @tf.function
 def train_step(input, images):
     with tf.GradientTape() as map_tape, tf.GradientTape() as syn_tape, tf.GradientTape() as disc_tape:
-        dlatents = mapping_ave(input, training = True)
-        g_images = synthesis_ave(dlatents, training = True)
+        dlatents = mapping(input, training = True)
+        g_images = synthesis(dlatents, training = True)
 
         real_output = discriminator(images,   training=True)
         fake_output = discriminator(g_images, training=True)
@@ -47,8 +47,8 @@ def train_step(input, images):
 
 
     #apply gradients
-    gradients_of_mapping       = map_tape.gradient(loss_gen,   mapping_ave.trainable_variables)
-    gradients_of_synthetis     = syn_tape.gradient(loss_gen,   synthesis_ave.trainable_variables)
+    gradients_of_mapping       = map_tape.gradient(loss_gen,   mapping.trainable_variables)
+    gradients_of_synthetis     = syn_tape.gradient(loss_gen,   synthesis.trainable_variables)
     gradients_of_discriminator = disc_tape.gradient(loss_disc, discriminator.trainable_variables)
 
     gradients_of_mapping       = [g if g is not None else tf.zeros_like(g) for g in gradients_of_mapping ]
@@ -100,7 +100,12 @@ def train(dataset, GEN_LR, DIS_LR, train_summary_writer):
 
 
     #save first images
-    generate_and_save_images(mapping_ave, synthesis_ave, input_latent, 0)
+    div = generate_and_save_images(mapping_ave, synthesis_ave, input_latent, 0)
+    with train_summary_writer.as_default():
+        for res in range(RES_LOG2-1):
+            pow = 2**(res+2)
+            var_name = "divergence/" + str(pow) + "x" + str(pow)
+            tf.summary.scalar(var_name, div[res], step=0)
 
     tstart = time.time()
     tint   = tstart
@@ -109,17 +114,24 @@ def train(dataset, GEN_LR, DIS_LR, train_summary_writer):
         image_batch = next(iter(dataset))
         mtr = train_step(input_batch, image_batch)
 
+        config = lr_gen_schedule.get_config()
+        if config["staircase"]:
+            p = tf.floor(it/config["decay_steps"])
+            lr_gen = tf.multiply(config["initial_learning_rate"], tf.pow(config["decay_rate"], p))
+
         #print losses
         if it % PRINT_EVERY == 0:
             tend = time.time()
             print ('Total time {0:3.1f} h, Iteration {1:8d}, Time Step {2:6.1f} s, ' \
-                'loss_disc {3:6.1e}, '  \
-                'loss_gen {4:6.1e}, '   \
+                'loss_disc {3:6.1e}, ' \
+                'loss_gen {4:6.1e}, ' \
                 'r1_penalty {5:6.1e}, ' \
+                'lr_gen {6:6.1e}, ' \
                 .format((tend-tstart)/3600, it, tend-tint, \
                 mtr[0], \
                 mtr[1], \
-                mtr[2]))
+                mtr[2], \
+                lr_gen))
             tint = tend
 
             # write losses tensorboard
@@ -141,12 +153,6 @@ def train(dataset, GEN_LR, DIS_LR, train_summary_writer):
         if (it+1) % SAVE_EVERY == 0:    
             checkpoint.save(file_prefix = CHKP_PREFIX)
 
-        #reduce learning rate
-        if (it+1) % REDUCE_EVERY == 0:    
-            # reduce learning rate
-            if (GEN_LR>LR_THRS):
-                GEN_LR = GEN_LR*1.0e-1
-                DIS_LR = GEN_LR
 
     print("Total divergencies for each resolution are:")
     for res in range(RES_LOG2-1):