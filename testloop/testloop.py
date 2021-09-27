17774093dcbdda6f99fafd0086926829ebd9733b
Jony Castagna <jony.castagna@stfc.ac.uk>
2021-09-27 21:30:32 +0100

    Renamed compare file

--- testloop/testloop.py
+++ testloop/testloop.py
@@ -15,7 +15,7 @@ for name,dr in zip(list_case_name, list_case_dr):
 
     cmd1 = "sed -i 's/CASE_NAME/"       + name    + "/g' parameters.py"
     cmd2 = "sed -i 's/CASE_DECAY_RATE/" + str(dr) + "/g' parameters.py"
-    cmd3 = "sed -i 's/CASE_NAME/"       + name    + "/g' utilities/compare.py"
+    cmd3 = "sed -i 's/CASE_NAME/"       + name    + "/g' utilities/compare_images.py"
 
     os.system(cmd1)
     os.system(cmd2)
@@ -26,7 +26,7 @@ for name,dr in zip(list_case_name, list_case_dr):
 
     cmd1 = "sed -i 's/"                  + name    + "/CASE_NAME/g' parameters.py"
     cmd2 = "sed -i 's/DECAY_RATE     = " + str(dr) + "/DECAY_RATE     = CASE_DECAY_RATE/g' parameters.py"
-    cmd3 = "sed -i 's/"                  + name    + "/CASE_NAME/g' utilities/compare.py"
+    cmd3 = "sed -i 's/"                  + name    + "/CASE_NAME/g' utilities/compare_images.py"
 
     os.system(cmd1)
     os.system(cmd2)
