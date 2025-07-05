Fixed scaling. It runs now up to 2000 time units the 128x128

LES_Solvers/LES_plot.py
@@ -191,7 +191,7 @@ def print_fields(U_, V_, P_, W_, N, filename, \
 
 def print_fields_3(U_, V_, P_, geomR=None, geomZ=None, N=None, filename=None, testcase='HW', \
     Umin=None, Umax=None, Vmin=None, Vmax=None, Pmin=None, Pmax=None, \
-    labels=[r'$n$', r'$\phi$', r'$\zeta$'], plot=False, dpi=100, transpose=True):
+    labels=[r'$n$', r'$\phi$', r'$\zeta$'], plot=False, dpi=100, transpose=True, plotCenterlines=True, legend=True, showAxis=True):
 
     if (testcase=='HIT_2D'):
         labelR = labels[0]
@@ -217,15 +217,21 @@ def print_fields_3(U_, V_, P_, geomR=None, geomZ=None, N=None, filename=None, te
     N = len(U[0,:])
 
     #---------------------------------- plot surfaces
-    fig, axs = plt.subplots(2, 3, figsize=(20,10), dpi=dpi)
+    if (plotCenterlines):
+        fig, axs = plt.subplots(2, 3, figsize=(20,10), dpi=dpi)
+        ax1 = axs[0,0]
+        ax2 = axs[1,0]
+        ax3 = axs[0,1]
+        ax4 = axs[1,1]
+        ax5 = axs[0,2]
+        ax6 = axs[1,2]
+    else:
+        fig, axs = plt.subplots(1, 3, figsize=(20,10), dpi=dpi)
+        ax1 = axs[0]
+        ax3 = axs[1]
+        ax5 = axs[2]
     fig.subplots_adjust(hspace=0.25)
 
-    ax1 = axs[0,0]
-    ax2 = axs[1,0]
-    ax3 = axs[0,1]
-    ax4 = axs[1,1]
-    ax5 = axs[0,2]
-    ax6 = axs[1,2]
 
     cmap1 = 'Blues'
     cmap2 = 'Reds_r'
@@ -245,28 +251,40 @@ def print_fields_3(U_, V_, P_, geomR=None, geomZ=None, N=None, filename=None, te
         velx = ax1.pcolormesh(geomR, geomZ, U, cmap=cmap1, edgecolors='k', linewidths=0.1, shading='gouraud', vmin=Umin, vmax=Umax)
     else:
         velx = ax1.pcolormesh(U, cmap=cmap1, edgecolors='k', linewidths=0.1, shading='gouraud', vmin=Umin, vmax=Umax)
-        
-    fig.colorbar(velx, ax=ax1)
-    ax1.title.set_text(labelR)
-    ax1.set_aspect(1)
-
+    
     if (geomR is not None):
         vely = ax3.pcolormesh(geomR, geomZ, V, cmap=cmap2, edgecolors='k', linewidths=0.1, shading='gouraud', vmin=Vmin, vmax=Vmax)
     else:
         vely = ax3.pcolormesh(V, cmap=cmap2, edgecolors='k', linewidths=0.1, shading='gouraud', vmin=Vmin, vmax=Vmax)
-        
-    fig.colorbar(vely, ax=ax3)
-    ax3.title.set_text(labelG)
-    ax3.set_aspect(1)
 
     if (geomR is not None):
         pres = ax5.pcolormesh(geomR, geomZ, P, cmap=cmap3, edgecolors='k', linewidths=0.1, shading='gouraud', vmin=Pmin, vmax=Pmax)
     else:
         pres = ax5.pcolormesh(P, cmap=cmap3, edgecolors='k', linewidths=0.1, shading='gouraud', vmin=Pmin, vmax=Pmax)
+
+    if (legend):
+        fig.colorbar(velx, ax=ax1)
+        fig.colorbar(vely, ax=ax3)
+        fig.colorbar(pres, ax=ax5)
+
+    if (not showAxis):
+        ax1.get_xaxis().set_visible(False)
+        ax3.get_xaxis().set_visible(False)
+        ax5.get_xaxis().set_visible(False)
+        ax1.get_yaxis().set_visible(False)
+        ax3.get_yaxis().set_visible(False)
+        ax5.get_yaxis().set_visible(False)
         
-    fig.colorbar(pres, ax=ax5)
+    ax1.title.set_text(labelR)
+    ax1.set_aspect(1)
+
+    ax3.title.set_text(labelG)
+    ax3.set_aspect(1)
+
     ax5.title.set_text(labelB)
     ax5.set_aspect(1)
+
+
 
 
     colors = plt.cm.jet(np.linspace(10,1,21))
@@ -296,30 +314,31 @@ def print_fields_3(U_, V_, P_, geomR=None, geomZ=None, N=None, filename=None, te
 
 
     #---------------------------------- plot centerlines
-    if (dir==0):    # x-direction
-        x = list(range(N))
-        hdim = N//2
-        yU = U[hdim,:]
-        yV = V[hdim,:]
-        yP = P[hdim,:]
-    elif (dir==1):  # y-direction 
-        x = list(range(N))
-        hdim = N//2
-        yU = U[:,hdim]
-        yV = V[:,hdim]
-        yP = P[:,hdim]
+    if (plotCenterlines):
+        if (dir==0):    # x-direction
+            x = list(range(N))
+            hdim = N//2
+            yU = U[hdim,:]
+            yV = V[hdim,:]
+            yP = P[hdim,:]
+        elif (dir==1):  # y-direction 
+            x = list(range(N))
+            hdim = N//2
+            yU = U[:,hdim]
+            yV = V[:,hdim]
+            yP = P[:,hdim]
 
-    velx = ax2.plot(x, yU, color=lineColor)
-    ax2.set_ylim([Umin, Umax])
-    ax2.title.set_text(labelR)
+        velx = ax2.plot(x, yU, color=lineColor)
+        ax2.set_ylim([Umin, Umax])
+        ax2.title.set_text(labelR)
 
-    vely = ax4.plot(x, yV, color=lineColor)
-    ax4.set_ylim([Vmin, Vmax])
-    ax4.title.set_text(labelG)
+        vely = ax4.plot(x, yV, color=lineColor)
+        ax4.set_ylim([Vmin, Vmax])
+        ax4.title.set_text(labelG)
 
-    pres = ax6.plot(x, yP, color=lineColor)
-    ax6.set_ylim([Pmin, Pmax])
-    ax6.title.set_text(labelB)
+        pres = ax6.plot(x, yP, color=lineColor)
+        ax6.set_ylim([Pmin, Pmax])
+        ax6.title.set_text(labelB)
 
     # save images
     plt.suptitle(filename)
@@ -431,7 +450,7 @@ def print_fields_2(U_, V_, filename, Umin=None, Umax=None, Vmin=None, Vmax=None)
 
 
 
-def print_fields_1(W_, filename, Wmin=None, Wmax=None, legend=True):
+def print_fields_1(W_, filename=None, labels=None, Wmin=None, Wmax=None, legend=True):
     
     #---------------------------------- find vorticity
     W = convert(W_)
@@ -443,12 +462,15 @@ def print_fields_1(W_, filename, Wmin=None, Wmax=None, legend=True):
 
     vort = ax1.pcolormesh(W, cmap='hot', edgecolors='k', linewidths=0.1, shading='gouraud', vmin=Wmin, vmax=Wmax)
     ax1.set_aspect(1)
-    if (legend):
-        fig.colorbar(vort, ax=ax1)
-        ax1.title.set_text(r'\omega')
-        plt.suptitle(filename)
-    else:
-        ax1.axis("off")
+    #if (legend):
+        # fig.colorbar(vort, ax=ax1)
+        # if (labels==None):
+        #     ax1.title.set_text(r'$\zeta$')
+        # else:
+        #     ax1.title.set_text(labels)
+        #plt.suptitle(filename)
+
+    ax1.axis("off")
 
     # save images
     plt.savefig(filename, bbox_inches='tight', pad_inches=0)    

bout_interfaces/create_restart.py
@@ -99,7 +99,7 @@ time.sleep(3)
 
 
 # create filter model
-if (GAUSSIAN_FILTER):
+if (EXTERNAL_FILTER):
     x_in    = tf.keras.Input(shape=([NUM_CHANNELS, OUTPUT_DIM, OUTPUT_DIM]), dtype=DTYPE)
     out     = apply_filter_NCH(x_in, size=4*RS, rsca=RS, mean=0.0, delta=RS, type='Gaussian', NCH=NUM_CHANNELS)
     gfilter = tf.keras.Model(inputs=x_in, outputs=out)
@@ -209,32 +209,35 @@ if (LOAD_DNS):
         P_DNS = find_vorticity_HW(UVP_LES_org[:,1:2,:,:], DELX_LES, DELY_LES)
         UVP_LES_org = tf.concat([UVP_LES_org[:,0:2,:,:], P_DNS], axis=1)  
         
-    # filter image
-    rs = 2 
-    for reslog in range(RES_LOG2, RES_LOG2-FIL-1, -1):
-        res = 2**reslog
-        if (reslog==RES_LOG2):
-            fUVP_DNS, _ = normalize_max(UVP_DNS_org)
-        else:
-            fUVP_DNS = apply_filter_NCH(fUVP_DNS, size=4, rsca=rs, mean=0.0, delta=1.0, type='Gaussian', NCH=3)
-            U_DNS    = fUVP_DNS[:,0:1,:,:]
-            V_DNS    = fUVP_DNS[:,1:2,:,:]
-            if (CALC_VORTICITY):        
-                P_DNS  = apply_filter_NCH(V_DNS, size=2, rsca=1, mean=0.0, delta=DELX*OUTPUT_DIM/res, type='Vorticity', NCH=1)
-                fUVP_DNS = tf.concat([U_DNS, V_DNS, P_DNS], axis=1)
-            else:
-                P_DNS    = fUVP_DNS[:,2:3,:,:]
-            fUVP_DNS = find_centred_fields(fUVP_DNS)
-            fUVP_DNS, _ = normalize_max(fUVP_DNS)
+    # # filter image
+    # rs = 2 
+    # for reslog in range(RES_LOG2, RES_LOG2-FIL-1, -1):
+    #     res = 2**reslog
+    #     if (reslog==RES_LOG2):
+    #         fUVP_DNS, _ = normalize_max(UVP_DNS_org)
+    #     else:
+    #         fUVP_DNS = apply_filter_NCH(fUVP_DNS, size=4, rsca=rs, mean=0.0, delta=1.0, type='Gaussian', NCH=3)
+    #         U_DNS    = fUVP_DNS[:,0:1,:,:]
+    #         V_DNS    = fUVP_DNS[:,1:2,:,:]
+    #         if (CALC_VORTICITY):        
+    #             P_DNS  = apply_filter_NCH(V_DNS, size=2, rsca=1, mean=0.0, delta=DELX*OUTPUT_DIM/res, type='Vorticity', NCH=1)
+    #             fUVP_DNS = tf.concat([U_DNS, V_DNS, P_DNS], axis=1)
+    #         else:
+    #             P_DNS    = fUVP_DNS[:,2:3,:,:]
+    #         fUVP_DNS = find_centred_fields(fUVP_DNS)
+    #         fUVP_DNS, _ = normalize_max(fUVP_DNS)
 
-        # normalize the data
-        fUVP_DNS, _ = normalize_max(fUVP_DNS)
+    #     # normalize the data
+    #     fUVP_DNS, _ = normalize_max(fUVP_DNS)
         
     # save LES_in0
-    LES_in0 = tf.identity(fUVP_DNS)
+    # LES_in0 = tf.identity(fUVP_DNS)
+    LES_in0, _ = normalize_max(UVP_LES_org)
 
-    fnUVPo, nfUVPo, fUVP_amaxo, nUVP_amaxo = find_scaling(UVP_DNS_org, gfilter_1ch, subSection=False, findNewValues=False)
-    UVP_max = [nUVP_amaxo] + [fUVP_amaxo]
+    # find min/max for DNS and LES
+    _, UVP_amaxo  = normalize_max(UVP_DNS_org)
+    _, fUVP_amaxo = normalize_max(UVP_LES_org)
+    UVP_max = [UVP_amaxo] + [fUVP_amaxo]
 
 else:
 
@@ -266,18 +269,21 @@ else:
             sinDNS = sinLES*RS
             LES_in0 = tf.concat([LES_in0, tr(LES_in0_init, 0, sinLES)], axis=0)
 
-    UVP_DNS, _, _ = find_predictions(synthesis, gfilter, zAll, UVP_max)
-    
-    fnUVPo, nfUVPo, fUVP_amaxo, nUVP_amaxo = find_scaling(UVP_DNS, gfilter_1ch, subSection=False, findNewValues=False)
-    UVP_max = [nUVP_amaxo] + [fUVP_amaxo]
-        
-    UVP_DNS_org, UVP_LES_org, fUVP_DNS = find_predictions(synthesis, gfilter, zAll, UVP_max)
+    UVP_DNS, UVP_LES, fUVP_DNS = find_predictions(synthesis, gfilter, zAll, UVP_max)
+
+    # find min/max for DNS and LES
+    _, UVP_amaxo  = normalize_max(UVP_DNS)
+    _, fUVP_amaxo = normalize_max(UVP_LES)
+    UVP_max = [UVP_amaxo] + [fUVP_amaxo]
+
+print("UVP_max: ", UVP_max)
+
 
 
 # find min/max values
-Umax = abs(tf.reduce_max(nUVP_amaxo[:,0,:,:]).numpy())
-Vmax = abs(tf.reduce_max(nUVP_amaxo[:,1,:,:]).numpy())
-Pmax = abs(tf.reduce_max(nUVP_amaxo[:,2,:,:]).numpy())
+Umax = abs(tf.reduce_max(UVP_amaxo[:,0,:,:]).numpy())
+Vmax = abs(tf.reduce_max(UVP_amaxo[:,1,:,:]).numpy())
+Pmax = abs(tf.reduce_max(UVP_amaxo[:,2,:,:]).numpy())
 Umin = -Umax
 Vmin = -Vmax
 Pmin = -Pmax
@@ -303,11 +309,11 @@ print_fields_3(U_LES, V_LES, P_LES, filename=filename, testcase=TESTCASE, \
 
 dVdx = (-cr(V_DNS, 2, 0) + 8*cr(V_DNS, 1, 0) - 8*cr(V_DNS, -1,  0) + cr(V_DNS, -2,  0))/(12.0*DELX)
 dVdy = (-cr(V_DNS, 0, 2) + 8*cr(V_DNS, 0, 1) - 8*cr(V_DNS,  0, -1) + cr(V_DNS,  0, -2))/(12.0*DELY)
-plot_spectrum_2d_3v(U_DNS, dVdx, dVdy, L, filename_spectra, label="DNS(org)", close=False)
+plot_spectrum_2d_3v(U_DNS, dVdx, dVdy, L, filename_spectra, label="DNS(org)", close=False, original=True)
 
 dVdx = (-cr(V_LES, 2, 0) + 8*cr(V_LES, 1, 0) - 8*cr(V_LES, -1,  0) + cr(V_LES, -2,  0))/(12.0*DELX_LES)
 dVdy = (-cr(V_LES, 0, 2) + 8*cr(V_LES, 0, 1) - 8*cr(V_LES,  0, -1) + cr(V_LES,  0, -2))/(12.0*DELY_LES)
-plot_spectrum_2d_3v(U_LES, dVdx, dVdy, L, filename_spectra, label="LES(org)", close=False)
+plot_spectrum_2d_3v(U_LES, dVdx, dVdy, L, filename_spectra, label="LES(org)", close=False, original=True)
 
 
 print("============================Set reference DNS and LES")
@@ -328,15 +334,15 @@ if (RESTART_WL):
     z0         = data["z0"]
     dlatents   = data["dlatents"]
     LES_in0    = data["LES_in0"]
-    nUVP_amaxo = data["nUVP_amaxo"]
+    UVP_amaxo  = data["UVP_amaxo"]
     fUVP_amaxo = data["fUVP_amaxo"]
     
-    UVP_max = [nUVP_amaxo] + [fUVP_amaxo]
+    UVP_max = [UVP_amaxo] + [fUVP_amaxo]
     
     print("z0",                 z0.shape, np.min(z0),         np.max(z0))
     print("dlatents",     dlatents.shape, np.min(dlatents),   np.max(dlatents))
     print("LES_in0",       LES_in0.shape, np.min(LES_in0),    np.max(LES_in0))
-    print("nUVP_amaxo", nUVP_amaxo.shape, np.min(nUVP_amaxo), np.max(nUVP_amaxo))
+    print("UVP_amaxo",   UVP_amaxo.shape, np.min(UVP_amaxo),  np.max(UVP_amaxo))
     print("fUVP_amaxo", fUVP_amaxo.shape, np.min(fUVP_amaxo), np.max(fUVP_amaxo))        
 
     # assign variables
@@ -484,7 +490,7 @@ if (not RESTART_WL):
                 z0         = z0, \
                 dlatents   = dlatents, \
                 LES_in0    = LES_all[-1], \
-                nUVP_amaxo = nUVP_amaxo, \
+                UVP_amaxo  = UVP_amaxo, \
                 fUVP_amaxo = fUVP_amaxo, \
                 noise_DNS  = noise_DNS)
     else:
@@ -493,7 +499,7 @@ if (not RESTART_WL):
                 z0         = z0, \
                 dlatents   = dlatents, \
                 LES_in0    = LES_all[-1], \
-                nUVP_amaxo = nUVP_amaxo, \
+                UVP_amaxo  = UVP_amaxo, \
                 fUVP_amaxo = fUVP_amaxo)
 
 
@@ -618,7 +624,7 @@ df_DNS = (gfilter_1ch(df_DNS[tf.newaxis,tf.newaxis,:,:]))[0,0,:,:]
 
 c_LES = 10.0*fU_DNS
 l_LES = fU_DNS + fV_DNS
-if (GAUSSIAN_FILTER):
+if (EXTERNAL_FILTER):
     fP_DNS_noSca = (gfilter_noScaling(P_DNS[tf.newaxis,tf.newaxis,:,:]))[0,0,:,:]   # the downscaling must happens after the filtering!!
     d_LES = ((cr(fP_DNS_noSca, 1, 0) - cr(fP_DNS_noSca,-1, 0))/(2*DELX)) + ((cr(fP_DNS_noSca, 0, 1) - cr(fP_DNS_noSca, 0,-1))/(2*DELY))
     d_LES = d_LES[::RS,::RS]

bout_interfaces/pBOUT.py
@@ -127,7 +127,7 @@ else:
 
 
 # create filter model
-if (GAUSSIAN_FILTER):
+if (EXTERNAL_FILTER):
     x_in    = tf.keras.Input(shape=([NUM_CHANNELS, OUTPUT_DIM, OUTPUT_DIM]), dtype=DTYPE)
     out     = apply_filter_NCH(x_in, size=4*RS, rsca=RS, mean=0.0, delta=RS, type='Gaussian')
     gfilter = tf.keras.Model(inputs=x_in, outputs=out)
@@ -166,15 +166,15 @@ if (RESTART_WL):
     z0         = data["z0"]
     dlatents   = data["dlatents"]
     LES_in0    = data["LES_in0"]
-    nUVP_amaxo = data["nUVP_amaxo"]
+    UVP_amaxo  = data["UVP_amaxo"]
     fUVP_amaxo = data["fUVP_amaxo"]
     
-    UVP_max = [nUVP_amaxo] + [fUVP_amaxo]
+    UVP_max = [UVP_amaxo] + [fUVP_amaxo]
     
     print("z0",                 z0.shape, np.min(z0),         np.max(z0))
     print("dlatents",     dlatents.shape, np.min(dlatents),   np.max(dlatents))
     print("LES_in0",       LES_in0.shape, np.min(LES_in0),    np.max(LES_in0))
-    print("nUVP_amaxo", nUVP_amaxo.shape, np.min(nUVP_amaxo), np.max(nUVP_amaxo))
+    print("UVP_amaxo",   UVP_amaxo.shape, np.min(UVP_amaxo),  np.max(UVP_amaxo))
     print("fUVP_amaxo", fUVP_amaxo.shape, np.min(fUVP_amaxo), np.max(fUVP_amaxo))        
 
     # assign variables
@@ -220,9 +220,9 @@ U_DNS = UVP_DNS[0,0,:,:].numpy()
 V_DNS = UVP_DNS[0,1,:,:].numpy()
 P_DNS = UVP_DNS[0,2,:,:].numpy()
 
-Umax = abs(tf.reduce_max(nUVP_amaxo[:,0,:,:]).numpy())
-Vmax = abs(tf.reduce_max(nUVP_amaxo[:,1,:,:]).numpy())
-Pmax = abs(tf.reduce_max(nUVP_amaxo[:,2,:,:]).numpy())
+Umax = abs(tf.reduce_max(UVP_amaxo[:,0,:,:]).numpy())
+Vmax = abs(tf.reduce_max(UVP_amaxo[:,1,:,:]).numpy())
+Pmax = abs(tf.reduce_max(UVP_amaxo[:,2,:,:]).numpy())
 Umin = -Umax
 Vmin = -Vmax
 Pmin = -Pmax
@@ -238,21 +238,26 @@ print("\nInitial residuals ------------------------:     resREC {0:3e} resLES {1
 imgA = tf.identity(UVP_DNS)
 if (USE_DIFF_LES):
     fimgA = tf.identity(fUVP_DNS)
-    nfimgA, _ = normalize_max(fUVP_DNS)
+    nfimgA, fUVP_amax = normalize_max(fUVP_DNS)
 else:
     fimgA = tf.identity(UVP_LES)
-    nfimgA, _ = normalize_max(UVP_LES)
+    nfimgA, fUVP_amax = normalize_max(UVP_LES)
 
 
 
-# # set old scaling coefficients
-fnUVPo, nfUVPo, fUVP_amaxo, nUVP_amaxo = find_scaling(UVP_DNS, gfilter_sub, findNewValues=False)
-UVP_max = [nUVP_amaxo] + [fUVP_amaxo]
+# set old scaling coefficients
+nUVP_DNS, UVP_amaxo = normalize_max(UVP_DNS)
+fnUVPo              = gfilter(nUVP_DNS)[:,:,N2R:N2R+1,N2R:N2R+1]
+nfUVPo              = nfimgA[:,:,N2R:N2R+1,N2R:N2R+1]
+fUVP_amaxo          = fUVP_amax
+UVP_maxo            = [UVP_amaxo] + [fUVP_amaxo]
+
+print("UVP_max: ", UVP_maxo)
+
 
 # prepare old LES_in values
 if (USE_DIFF_LES):
     LES_in0o = tf.identity(LES_in0)
-    nfimgAo = tf.identity(nfimgA)
 
 file = open("./data/BOUT.inp", 'r')
 for line in file:
@@ -282,6 +287,7 @@ X, Y, Z = np.meshgrid(x, y, z, indexing='ij')
 print("Grid values NX,NY,NZ,LX,LY,LZ", NX,NY,NZ,LX,LY,LZ)
 
 
+minmaxTot = []
 
 #------------------------------------------------------ initialize the flow
 def initFlow(npv):
@@ -363,9 +369,12 @@ def initFlow(npv):
 def findLESTerms(pLES):
 
     global UVP_DNS
-    global fnUVPo, nfUVPo, fUVP_amaxo, nUVP_amaxo
+    global fnUVPo, nfUVPo, fUVP_amaxo, UVP_amaxo, nfimgAo, UVP_maxo
     global pPrint, simtimeo, pStepo
-    # global LES_in0o, nfimgAo
+    global minmaxTot
+    
+    if (USE_DIFF_LES):
+        global LES_in0o
 
 
 
@@ -410,42 +419,24 @@ def findLESTerms(pLES):
         
 
         #--------------------------- prepare LES field in 
-        # normalize
-        U_min = np.min(fU)
-        U_max = np.max(fU)
-        V_min = np.min(fV)
-        V_max = np.max(fV)
-        P_min = np.min(fP)
-        P_max = np.max(fP)
-
-        fU_amax = max(np.absolute(U_min), np.absolute(U_max))
-        fV_amax = max(np.absolute(V_min), np.absolute(V_max))
-        fP_amax = max(np.absolute(P_min), np.absolute(P_max))
-
-        nfU = fU/fU_amax
-        nfV = fV/fV_amax
-        nfP = fP/fP_amax
-        nfU = tf.convert_to_tensor(nfU, dtype=DTYPE)
-        nfV = tf.convert_to_tensor(nfV, dtype=DTYPE)
-        nfP = tf.convert_to_tensor(nfP, dtype=DTYPE)
-        nfU = tf.transpose(nfU, [1,0,2])
-        nfV = tf.transpose(nfV, [1,0,2])
-        nfP = tf.transpose(nfP, [1,0,2])
-        nfU = nfU[:,tf.newaxis,:,:]
-        nfV = nfV[:,tf.newaxis,:,:]
-        nfP = nfP[:,tf.newaxis,:,:]
-        nfimgA = tf.concat([nfU,nfV,nfP], axis=1)
+        fU = tf.convert_to_tensor(fU, dtype=DTYPE)
+        fV = tf.convert_to_tensor(fV, dtype=DTYPE)
+        fP = tf.convert_to_tensor(fP, dtype=DTYPE)
+        fU = tf.transpose(fU, [1,0,2])
+        fV = tf.transpose(fV, [1,0,2])
+        fP = tf.transpose(fP, [1,0,2])
+        fU = fU[:,tf.newaxis,:,:]
+        fV = fV[:,tf.newaxis,:,:]
+        fP = fP[:,tf.newaxis,:,:]
+        fimgA = tf.concat([fU, fV, fP], axis=1)
+        nfimgA, fUVP_amax = normalize_max(fimgA)
+        nfUVP = nfimgA[:,:,N2R:N2R+1,N2R:N2R+1]
 
         # set new LES_in
         if (USE_DIFF_LES):
             LES_in0     = LES_in0o*nfimgA/nfimgAo
             LES_in0, _  = normalize_max(LES_in0)
             LES_in0o    = tf.identity(LES_in0)
-            nfimgAo     = tf.identity(nfimgA)
-
-        # find new scaling
-        fnUVPo, nfUVPo, fUVP_amaxo, nUVP_amaxo = find_scaling(UVP_DNS, gfilter_sub, fnUVPo, nfUVPo, fUVP_amaxo, nUVP_amaxo)
-        UVP_max = [nUVP_amaxo] + [fUVP_amaxo]
 
         # end prepare phase
         if (PROFILE_BOUT):
@@ -465,10 +456,18 @@ def findLESTerms(pLES):
 
     #--------------------------- find reconstructed field
     with nvtx.annotate("prediction", color="purple"):
-        UVP_DNS = find_predictions(synthesis, gfilter, zAll, UVP_max, find_fDNS=False)
+        UVP_DNS, fnUVPo, nfUVPo, UVP_maxo = find_predictions(synthesis, gfilter, zAll, UVP_maxo, nfUVP, fUVP_amax, fnUVPo, nfUVPo, find_fDNS=False, rescale=True)
         # resREC, resLES, resDNS, loss_fil = find_residuals(UVP_DNS, UVP_LES, fUVP_DNS, UVP_DNS, UVP_LES, typeRes=0)
         # print("Starting residuals: step {0:6d} simtime {1:3e} resREC {2:3e} resLES {3:3e} resDNS {4:3e} loss_fil {5:3e}" \
         #     .format(pStep, simtime, resREC.numpy(), resLES.numpy(), resDNS.numpy(), loss_fil))
+
+        minmaxTot.append([UVP_maxo[0][0,0,0,0].numpy(), UVP_maxo[0][0,1,0,0].numpy(), UVP_maxo[0][0,2,0,0].numpy()])
+        if (pStep%100==0):
+            plt.plot(minmaxTot)
+            plt.savefig("./results_StylES/minmaxTot.png")
+            plt.close()
+        # filename = "./results_StylES/minmaxTot.txt"
+        # np.savetxt(filename, minmaxTot, fmt='%1.4e')
 
 
     if (PROFILE_BOUT):

functions.py
@@ -393,37 +393,56 @@ def apply_filter(field, size=1, rsca=1, mean=0.0, delta=1.0, type='Gaussian', su
 def apply_filter_NCH(field, size=1, rsca=1, mean=0.0, delta=1.0, type='Gaussian', subsection=False, NCH=NUM_CHANNELS):
 
     # prepare differential Kernel
-    if (type=='Top-hat'):
-        filter_kernel = make_tophat_kernel(size=size, mean=mean, delta=delta)
-    elif (type=='Gaussian'):
-        filter_kernel = make_gaussian_kernel(size=size, mean=mean, delta=delta)
-    elif (type=='Spectral'):
-        filter_kernel = make_spectral_kernel(size=size, mean=mean, delta=delta)
-    elif (type=='Differential'):
-        filter_kernel = make_differential_kernel(size=size, mean=mean, delta=delta)
-    elif (type=='Vorticity'):
-        filter_kernel = make_vorticity_kernel(delta=delta)
-    filter_kernel = filter_kernel[:, :, tf.newaxis, tf.newaxis]
-    filter_kernel = tf.tile(filter_kernel, [1,1,1,NCH])
-    filter_kernel = tf.cast(filter_kernel, dtype=field.dtype)
+    if (type=='Differential'):
 
-    # add padding
-    if (subsection):
-        field = tf.nn.conv2d(field, filter_kernel, strides=[1, 1, rsca, rsca], padding="SAME", data_format=data_format)
-        N2 = int(field.shape[-1]/2)
-        return field[:,:,N2:N2+1,N2:N2+1]
-    else:
-        pleft   = size
-        pright  = size
-        ptop    = size
-        pbottom = size
+        # Build filter kernel in spectral space
+        denom = 1.0 + delta**2 * (KX2 + KY2)
 
-        field = periodic_padding_flexible(field, axis=(2,3), padding=([pleft, pright], [ptop, pbottom]))
+        # Forward FFT
+        u_hat = tf.signal.fft2d(tf.cast(field, tf.complex128))
 
-        # convolve
-        field = tf.nn.conv2d(field, filter_kernel, strides=[1, 1, rsca, rsca], padding="VALID", data_format=data_format)
+        # Apply filter
+        u_hat_filtered = u_hat / tf.cast(denom, tf.complex128)
 
+        # Inverse FFT
+        u_les = tf.math.real(tf.signal.ifft2d(u_hat_filtered))
+
+        # downsmaple
+        field = u_les[:,:,::rsca,::rsca]
+        
         return field
+
+    else:
+
+        if (type=='Top-hat'):
+            filter_kernel = make_tophat_kernel(size=size, mean=mean, delta=delta)
+        elif (type=='Gaussian'):
+            filter_kernel = make_gaussian_kernel(size=size, mean=mean, delta=delta)
+        elif (type=='Spectral'):
+            filter_kernel = make_spectral_kernel(size=size, mean=mean, delta=delta)
+        elif (type=='Vorticity'):
+            filter_kernel = make_vorticity_kernel(delta=delta)
+        filter_kernel = filter_kernel[:, :, tf.newaxis, tf.newaxis]
+        filter_kernel = tf.tile(filter_kernel, [1,1,1,NCH])
+        filter_kernel = tf.cast(filter_kernel, dtype=field.dtype)
+
+        # add padding
+        if (subsection):
+            field = tf.nn.conv2d(field, filter_kernel, strides=[1, 1, rsca, rsca], padding="SAME", data_format=data_format)
+            N2 = int(field.shape[-1]/2)
+            return field[:,:,N2:N2+1,N2:N2+1]
+        else:
+            pleft   = size
+            pright  = size
+            ptop    = size
+            pbottom = size
+
+            field = periodic_padding_flexible(field, axis=(2,3), padding=([pleft, pright], [ptop, pbottom]))
+
+            # convolve
+            field = tf.nn.conv2d(field, filter_kernel, strides=[1, 1, rsca, rsca], padding="VALID", data_format=data_format)
+
+            return field
     
 
 
@@ -906,7 +925,7 @@ def find_centred_fields(UVP):
 
 
 @tf.function
-def find_predictions(synthesis, filter, z, UVP_max, find_fDNS=True):
+def find_predictions(synthesis, filter, z, UVP_max, nfUVP=None, fUVP_amax=None, fnUVPo=None, nfUVPo=None, find_fDNS=True, rescale=False):
 
     if (NUM_CHANNELS==1):
 
@@ -914,16 +933,23 @@ def find_predictions(synthesis, filter, z, UVP_max, find_fDNS=True):
         LES_U = z[2][:,0:1,:,:]
         LES_V = z[2][:,1:2,:,:]
 
-        _, U_DNS = synthesis([z[0], z[1], LES_U], training=False)
-        _, V_DNS = synthesis([z[0], z[1], LES_V], training=False)
+        _, nU_DNS = synthesis([z[0], z[1], LES_U], training=False)
+        _, nV_DNS = synthesis([z[0], z[1], LES_V], training=False)
+        nP_DNS    = find_vorticity_HW(nV_DNS, DELX_LES, DELY_LES)  # to be fixed for rescaling...
+        nUVP_DNS  = tf.concat([nU_DNS, nV_DNS, nP_DNS], axis=1)
+
+        # to be fixed for rescaling...
 
         # rescale
-        U_DNS = U_DNS*UVP_max[0][:,0:1,:,:]
-        V_DNS = V_DNS*UVP_max[0][:,1:2,:,:]
+        U_DNS = nU_DNS*UVP_max[0][:,0:1,:,:]
+        V_DNS = nV_DNS*UVP_max[0][:,1:2,:,:]
 
         # find vorticity
-        P_DNS = find_vorticity_HW(V_DNS, DELX, DELY)
-        UVP_DNS = tf.concat([U_DNS, V_DNS, P_DNS], axis=1)
+        P_DNS  = find_vorticity_HW(V_DNS,  DELX, DELY)
+        nP_DNS = find_vorticity_HW(nV_DNS, DELX, DELY)
+
+        UVP_DNS  = tf.concat([U_DNS,   V_DNS,  P_DNS], axis=1)
+        nUVP_DNS = tf.concat([nU_DNS, nV_DNS, nP_DNS], axis=1)
 
         # find filtered fields
         if (find_fDNS):
@@ -937,7 +963,7 @@ def find_predictions(synthesis, filter, z, UVP_max, find_fDNS=True):
             fP_DNS = find_vorticity_HW(fV_DNS, DELX_LES, DELY_LES)
             fUVP_DNS = tf.concat([fU_DNS, fV_DNS, fP_DNS], axis=1)
 
-            return UVP_DNS, UVP_LES, fUVP_DNS
+            return UVP_DNS, UVP_LES, fUVP_DNS, nUVP_DNS
         else:
             return UVP_DNS
 
@@ -945,10 +971,16 @@ def find_predictions(synthesis, filter, z, UVP_max, find_fDNS=True):
 
         # find predictions
         predictions = synthesis(z, training=False)
-        UVP_DNS = predictions[RES_LOG2-2]
+        nUVP_DNS = predictions[RES_LOG2-2]
+
+        # find scaling
+        if (rescale):
+            fnUVP = filter(nUVP_DNS)[:,:,N2R:N2R+1,N2R:N2R+1]
+            UVP_amax = find_scaling(nfUVP, fnUVP, fnUVPo, nfUVPo, fUVP_amax, UVP_max[0], UVP_max[1])
+            UVP_max = [UVP_amax] + [fUVP_amax] 
 
         # rescale
-        UVP_DNS = rescale_max(UVP_DNS, UVP_max[0])
+        UVP_DNS = rescale_max(nUVP_DNS, UVP_max[0])
         U_DNS   = UVP_DNS[:,0:1,:,:]
         V_DNS   = UVP_DNS[:,1:2,:,:]
         
@@ -975,10 +1007,15 @@ def find_predictions(synthesis, filter, z, UVP_max, find_fDNS=True):
                 fP_DNS   = fUVP_DNS[:,2:3,:,:]
             fUVP_DNS = tf.concat([fU_DNS, fV_DNS, fP_DNS], axis=1)
 
-            return UVP_DNS, UVP_LES, fUVP_DNS
+            if (rescale):
+                return UVP_DNS, UVP_LES, fUVP_DNS, fnUVP, nfUVP, UVP_max, UVP_max
+            else:
+                return UVP_DNS, UVP_LES, fUVP_DNS
         else:
-            return UVP_DNS
-
+            if (rescale):
+                return UVP_DNS, fnUVP, nfUVP, UVP_max
+            else:
+                return UVP_DNS
 
 
 
@@ -992,16 +1029,18 @@ def wl_find_predictions(synthesis, filter, z, UVP_max, find_fDNS=True):
         LES_U = z[3][:,0:1,:,:]
         LES_V = z[3][:,1:2,:,:]
 
-        _, U_DNS = synthesis([z[0], z[1], z[2], LES_U], training=False)
-        _, V_DNS = synthesis([z[0], z[1], z[2], LES_V], training=False)
+        _, nU_DNS = synthesis([z[0], z[1], z[2], LES_U], training=False)
+        _, nV_DNS = synthesis([z[0], z[1], z[2], LES_V], training=False)
 
         # rescale
-        U_DNS = U_DNS*UVP_max[0][:,0:1,:,:]
-        V_DNS = V_DNS*UVP_max[0][:,1:2,:,:]
+        U_DNS = nU_DNS*UVP_max[0][:,0:1,:,:]
+        V_DNS = nV_DNS*UVP_max[0][:,1:2,:,:]
 
         # find vorticity
-        P_DNS = find_vorticity_HW(V_DNS, DELX, DELY)
-        UVP_DNS = tf.concat([U_DNS, V_DNS, P_DNS], axis=1)
+        P_DNS  = find_vorticity_HW(V_DNS, DELX, DELY)
+        nP_DNS = find_vorticity_HW(nV_DNS, DELX, DELY)
+        UVP_DNS  = tf.concat([U_DNS, V_DNS, P_DNS], axis=1)
+        nUVP_DNS = tf.concat([nU_DNS, nV_DNS, nP_DNS], axis=1)
 
         # find filtered fields
         if (find_fDNS):
@@ -1015,18 +1054,18 @@ def wl_find_predictions(synthesis, filter, z, UVP_max, find_fDNS=True):
             fP_DNS = find_vorticity_HW(fV_DNS, DELX_LES, DELY_LES)
             fUVP_DNS = tf.concat([fU_DNS, fV_DNS, fP_DNS], axis=1)
 
-            return UVP_DNS, UVP_LES, fUVP_DNS
+            return UVP_DNS, UVP_LES, fUVP_DNS, nUVP_DNS
         else:
-            return UVP_DNS
+            return UVP_DNS, nUVP_DNS
 
     else:
 
         # find predictions
         predictions = synthesis(z, training=False)
-        UVP_DNS = predictions[RES_LOG2-2]
+        nUVP_DNS = predictions[RES_LOG2-2]
 
         # rescale
-        UVP_DNS = rescale_max(UVP_DNS, UVP_max[0])
+        UVP_DNS = rescale_max(nUVP_DNS, UVP_max[0])
         U_DNS   = UVP_DNS[:,0:1,:,:]
         V_DNS   = UVP_DNS[:,1:2,:,:]
         
@@ -1053,9 +1092,9 @@ def wl_find_predictions(synthesis, filter, z, UVP_max, find_fDNS=True):
                 fP_DNS   = fUVP_DNS[:,2:3,:,:]
             fUVP_DNS = tf.concat([fU_DNS, fV_DNS, fP_DNS], axis=1)
 
-            return UVP_DNS, UVP_LES, fUVP_DNS
+            return UVP_DNS, UVP_LES, fUVP_DNS, nUVP_DNS
         else:
-            return UVP_DNS
+            return UVP_DNS, nUVP_DNS
 
 
 @tf.function
@@ -1065,12 +1104,17 @@ def find_residuals(UVP_DNS, UVP_LES, fUVP_DNS, tDNS, tLES, typeRes=0):
         resDNS   = 1.0/INIT_SCA*tf.math.reduce_mean(tf.math.squared_difference(fUVP_DNS, tLES))
         resLES   = 1.0/INIT_SCA*tf.math.reduce_mean(tf.math.squared_difference(UVP_LES,  tLES))
         resREC   = resDNS + resLES
-        loss_fil = resDNS
+        loss_fil = 1.0/INIT_SCA*tf.math.reduce_mean(tf.math.squared_difference(fUVP_DNS, tLES))
     elif (typeRes==1):
-        resLES   = 1.0/INIT_SCA*tf.math.reduce_mean(tf.math.squared_difference(fUVP_DNS, tLES)) # remember to use fUVP_DNS rather than UVP_LES
-        resDNS   = 0.0*resLES                                                                   # otherwise the gradients will be zero!!
+        resDNS   = 1.0/INIT_SCA*tf.math.reduce_mean(tf.math.squared_difference(fUVP_DNS, tLES)) # remember to use fUVP_DNS rather than UVP_LES
+        resLES   = 0.0*resDNS                                                                   # otherwise the gradients will be zero!!
         resREC   = resDNS + resLES
-        loss_fil = resLES
+        loss_fil = 1.0/INIT_SCA*tf.math.reduce_mean(tf.math.squared_difference(fUVP_DNS, tLES))
+    elif (typeRes==2):
+        resDNS   = 1.0/INIT_SCA*tf.math.reduce_mean(tf.math.squared_difference(UVP_DNS, tDNS)) # remember to use fUVP_DNS rather than UVP_LES
+        resLES   = 0.0*resDNS                                                                   # otherwise the gradients will be zero!!
+        resREC   = resDNS + resLES
+        loss_fil = 1.0/INIT_SCA*tf.math.reduce_mean(tf.math.squared_difference(fUVP_DNS, tLES))
         
     return resREC, resLES, resDNS, loss_fil
 
@@ -1112,7 +1156,7 @@ def find_bracket(F, G, filter, spacingFactor):
     
 
 @tf.function
-def find_scaling(UVP, gfilter, fnUVPo=None, nfUVPo=None, fUVP_amaxo=None, nUVP_amaxo=None, subSection=True, findNewValues=True):
+def find_scaling_sub(UVP, gfilter, fnUVPo=None, nfUVPo=None, fUVP_amaxo=None, UVP_amaxo=None, subSection=True, findNewValues=True):
     
         U = UVP[:,0:1,:,:]
         V = UVP[:,1:2,:,:]
@@ -1126,18 +1170,18 @@ def find_scaling(UVP, gfilter, fnUVPo=None, nfUVPo=None, fUVP_amaxo=None, nUVP_a
         P_min = tf.abs(tf.reduce_min(P, axis=(1,2,3), keepdims=True))
         P_max = tf.abs(tf.reduce_max(P, axis=(1,2,3), keepdims=True))
 
-        nU_amax = tf.maximum(U_min, U_max)
-        nV_amax = tf.maximum(V_min, V_max)
-        nP_amax = tf.maximum(P_min, P_max)
+        U_amax = tf.maximum(U_min, U_max)
+        V_amax = tf.maximum(V_min, V_max)
+        P_amax = tf.maximum(P_min, P_max)
 
         if (subSection):
-            nU = U[:,:,N2L:N2R,N2L:N2R]/nU_amax
-            nV = V[:,:,N2L:N2R,N2L:N2R]/nV_amax
-            nP = P[:,:,N2L:N2R,N2L:N2R]/nP_amax
+            nU = U[:,:,N2L:N2R,N2L:N2R]/U_amax
+            nV = V[:,:,N2L:N2R,N2L:N2R]/V_amax
+            nP = P[:,:,N2L:N2R,N2L:N2R]/P_amax
         else:
-            nU = U/nU_amax
-            nV = V/nV_amax
-            nP = P/nP_amax
+            nU = U/U_amax
+            nV = V/V_amax
+            nP = P/P_amax
 
         fnU = gfilter(nU)
         fnV = gfilter(nV)
@@ -1175,15 +1219,105 @@ def find_scaling(UVP, gfilter, fnUVPo=None, nfUVPo=None, fUVP_amaxo=None, nUVP_a
         fUVP_amax = tf.concat([fU_amax, fV_amax, fP_amax], axis=1)
         
         if (findNewValues):
-            nU_amax = (fnUVPo[0]*nfUVP[0])/(fnUVP[0]*nfUVPo[0])*nUVP_amaxo[0,0,0,0]*fUVP_amax[0,0,0,0]/fUVP_amaxo[0,0,0,0]
-            nV_amax = (fnUVPo[1]*nfUVP[1])/(fnUVP[1]*nfUVPo[1])*nUVP_amaxo[0,1,0,0]*fUVP_amax[0,1,0,0]/fUVP_amaxo[0,1,0,0]
-            nP_amax = (fnUVPo[2]*nfUVP[2])/(fnUVP[2]*nfUVPo[2])*nUVP_amaxo[0,2,0,0]*fUVP_amax[0,2,0,0]/fUVP_amaxo[0,2,0,0]
-            nUVP_amax = tf.concat([nU_amax, nV_amax, nP_amax], axis=1)
+            U_amax = (fnUVPo[0]*nfUVP[0])/(fnUVP[0]*nfUVPo[0])*UVP_amaxo[0,0,0,0]*fUVP_amax[0,0,0,0]/fUVP_amaxo[0,0,0,0]
+            V_amax = (fnUVPo[1]*nfUVP[1])/(fnUVP[1]*nfUVPo[1])*UVP_amaxo[0,1,0,0]*fUVP_amax[0,1,0,0]/fUVP_amaxo[0,1,0,0]
+            P_amax = (fnUVPo[2]*nfUVP[2])/(fnUVP[2]*nfUVPo[2])*UVP_amaxo[0,2,0,0]*fUVP_amax[0,2,0,0]/fUVP_amaxo[0,2,0,0]
+            UVP_amax = tf.concat([U_amax, V_amax, P_amax], axis=1)
         else:
-            nUVP_amax = tf.concat([nU_amax, nV_amax, nP_amax], axis=1)
+            UVP_amax = tf.concat([U_amax, V_amax, P_amax], axis=1)
 
-        return fnUVP, nfUVP, fUVP_amax, nUVP_amax
+        return fnUVP, nfUVP, fUVP_amax, UVP_amax
                         
+   
+                   
+    
+
+@tf.function
+def find_scaling_old(UVP, gfilter, fnUVPo=None, nfUVPo=None, fUVP_amaxo=None, UVP_amaxo=None, findNewValues=True):
+    
+    
+        # find filtered normalized fields
+        U = UVP[:,0:1,:,:]
+        V = UVP[:,1:2,:,:]
+        P = UVP[:,2:3,:,:]
+
+        U_min = tf.abs(tf.reduce_min(U, axis=(1,2,3), keepdims=True))
+        U_max = tf.abs(tf.reduce_max(U, axis=(1,2,3), keepdims=True))
+        V_min = tf.abs(tf.reduce_min(V, axis=(1,2,3), keepdims=True))
+        V_max = tf.abs(tf.reduce_max(V, axis=(1,2,3), keepdims=True))
+        P_min = tf.abs(tf.reduce_min(P, axis=(1,2,3), keepdims=True))
+        P_max = tf.abs(tf.reduce_max(P, axis=(1,2,3), keepdims=True))
+
+        U_amax = tf.maximum(U_min, U_max)
+        V_amax = tf.maximum(V_min, V_max)
+        P_amax = tf.maximum(P_min, P_max)
+
+        nU = U/U_amax
+        nV = V/V_amax
+        nP = P/P_amax
+
+        fnU = gfilter(nU)
+        fnV = gfilter(nV)
+        fnP = gfilter(nP)
+
+
+        # find normalized filtered field
+        fU = gfilter(U, training=False)
+        fV = gfilter(V, training=False)
+        fP = gfilter(P, training=False)
+
+        U_min = tf.abs(tf.reduce_min(fU, axis=(1,2,3), keepdims=True))
+        U_max = tf.abs(tf.reduce_max(fU, axis=(1,2,3), keepdims=True))
+        V_min = tf.abs(tf.reduce_min(fV, axis=(1,2,3), keepdims=True))
+        V_max = tf.abs(tf.reduce_max(fV, axis=(1,2,3), keepdims=True))
+        P_min = tf.abs(tf.reduce_min(fP, axis=(1,2,3), keepdims=True))
+        P_max = tf.abs(tf.reduce_max(fP, axis=(1,2,3), keepdims=True))
+
+        fU_amax = tf.maximum(U_min, U_max)
+        fV_amax = tf.maximum(V_min, V_max)
+        fP_amax = tf.maximum(P_min, P_max)
+
+        nfU = fU/fU_amax
+        nfV = fV/fV_amax
+        nfP = fP/fP_amax
+
+
+        # concatenate all values
+        fnUVP = [fnU[:,:,N2R:N2R+1,N2R:N2R+1], fnV[:,:,N2R:N2R+1,N2R:N2R+1], fnP[:,:,N2R:N2R+1,N2R:N2R+1]]
+        nfUVP = [nfU[:,:,N2R:N2R+1,N2R:N2R+1], nfV[:,:,N2R:N2R+1,N2R:N2R+1], nfP[:,:,N2R:N2R+1,N2R:N2R+1]]
+        fUVP_amax = tf.concat([fU_amax, fV_amax, fP_amax], axis=1)
+        
+
+        # find new scaling factors
+        if (findNewValues):
+            #print((fnUVPo[0]*nfUVP[0])/(fnUVP[0]*nfUVPo[0]), UVP_amaxo[0,0,0,0]*fUVP_amax[0,0,0,0]/fUVP_amaxo[0,0,0,0])
+            U_amax = (fnUVPo[0]*nfUVP[0])/(fnUVP[0]*nfUVPo[0])*UVP_amaxo[0,0,0,0]*fUVP_amax[0,0,0,0]/fUVP_amaxo[0,0,0,0]
+            V_amax = (fnUVPo[1]*nfUVP[1])/(fnUVP[1]*nfUVPo[1])*UVP_amaxo[0,1,0,0]*fUVP_amax[0,1,0,0]/fUVP_amaxo[0,1,0,0]
+            P_amax = (fnUVPo[2]*nfUVP[2])/(fnUVP[2]*nfUVPo[2])*UVP_amaxo[0,2,0,0]*fUVP_amax[0,2,0,0]/fUVP_amaxo[0,2,0,0]
+            UVP_amax = tf.concat([U_amax, V_amax, P_amax], axis=1)
+        else:
+            UVP_amax = tf.concat([U_amax, V_amax, P_amax], axis=1)
+
+
+        return fnUVP, nfUVP, fUVP_amax, UVP_amax
+
+
+
+@tf.function
+def find_scaling(nfUVP, fnUVP, fnUVPo, nfUVPo, fUVP_amax, UVP_amaxo, fUVP_amaxo):
+
+    # U_amax = (fnUVPo[:,0:1,:,:]*nfUVP[0,0,0,0])/(fnUVP[0,0,0,0]*nfUVPo[0,0,0,0])*UVP_amaxo[0,0,0,0]*fUVP_amax[0,0,0,0]/fUVP_amaxo[0,0,0,0]
+    # V_amax = (fnUVPo[:,1:2,:,:]*nfUVP[0,1,0,0])/(fnUVP[0,1,0,0]*nfUVPo[0,1,0,0])*UVP_amaxo[0,1,0,0]*fUVP_amax[0,1,0,0]/fUVP_amaxo[0,1,0,0]
+    # P_amax = (fnUVPo[:,2:3,:,:]*nfUVP[0,2,0,0])/(fnUVP[0,2,0,0]*nfUVPo[0,2,0,0])*UVP_amaxo[0,2,0,0]*fUVP_amax[0,2,0,0]/fUVP_amaxo[0,2,0,0]
+
+    U_amax = UVP_amaxo[:,0:1,:,:]*fUVP_amax[0,0,0,0]/fUVP_amaxo[0,0,0,0]
+    V_amax = UVP_amaxo[:,1:2,:,:]*fUVP_amax[0,1,0,0]/fUVP_amaxo[0,1,0,0]
+    P_amax = UVP_amaxo[:,2:3,:,:]*fUVP_amax[0,2,0,0]/fUVP_amaxo[0,2,0,0]
+
+    UVP_amax = tf.concat([U_amax, V_amax, P_amax], axis=1)
+
+    return UVP_amax
+    
     
     
 class layer_wlatent_mLES(layers.Layer):
@@ -1192,17 +1326,14 @@ class layer_wlatent_mLES(layers.Layer):
 
         w_init = tf.random_normal_initializer(mean=0.5, stddev=0.0)
         self.m = tf.Variable(
-            initial_value=w_init(shape=[M_LAYERS, LATENT_SIZE], dtype=DTYPE),
+            initial_value=w_init(shape=[G_LAYERS-M_LAYERS, 1], dtype=DTYPE),
             trainable=True,
             name="latent_mLES"
         )
 
     def call(self, w0, w1):
-        wa = self.m*w0[:,0:M_LAYERS,:] + (1.0-self.m)*w1[:,0:M_LAYERS,:]
-        wb = wa[:,M_LAYERS-1:M_LAYERS,:]
-        wb = tf.tile(wb, [1,G_LAYERS-M_LAYERS,1])
-        wa = wa[:,0:M_LAYERS,:]
-        w  = tf.concat([wa,wb], axis=1)
+        wa = self.m*w0[:,M_LAYERS:G_LAYERS,:] + (1.0-self.m)*w1[:,M_LAYERS:G_LAYERS,:]
+        w  = tf.concat([w0[:,0:M_LAYERS,:],wa], axis=1)
         return w
 
 
@@ -1222,5 +1353,8 @@ def step_find_dlatents_mDNS(synthesis, filter, opt, z, tDNS, tLES, ltv, UVP_max,
 
         
     return UVP_DNS, UVP_LES, fUVP_DNS, resREC, resLES, resDNS, loss_fil
+
+
+
 
     

parameters.py
@@ -41,7 +41,7 @@ else:                                         # INFO = INFO messages are not pri
     tf.get_logger().setLevel("ERROR")
 
 SEED = 0
-SEED_RESTART = 3
+SEED_RESTART = 0
 
 tf.random.set_seed(seed=SEED)  # ideally this should be set on if DEBUG is true...
 
@@ -82,7 +82,7 @@ FMAP_BASE         = 8192    # Overall multiplier for the number of feature maps.
 FMAP_DECAY        = 1.0     # log2 feature map reduction when doubling the resolution.
 FMAP_MAX          = 512     # Maximum number of feature maps in any layer.
 RES_LOG2          = int(np.log2(OUTPUT_DIM))
-FIL               = 3 # number of layers below the DNS  
+FIL               = 1 # number of layers below the DNS  
 IFIL              = FIL-1  # number of layers below the DNS  
 G_LAYERS          = RES_LOG2*2 - 2  # Numer of layers  
 G_LAYERS_FIL      = (RES_LOG2-FIL)*2 - 2   # Numer of layers for the filter
@@ -101,7 +101,7 @@ AMP_INSTAN_NOISE  = 0.0
 # Training hyper-parameters
 TOT_ITERATIONS = 500000
 PRINT_EVERY    = 1000
-IMAGES_EVERY   = 10000
+IMAGES_EVERY   = 1000
 SAVE_EVERY     = 100000
 IRESTART       = False
 
@@ -144,7 +144,7 @@ NY2             = max(1,int(BATCH_SIZE/2))
 RS              = int(2**FIL)
 RS2             = int(RS/2)
 N2L             = N_LES2-RS2
-N2R             = N_LES2+RS2+1
+N2R             = N_LES2+RS2-10
 
 if (TESTCASE=="HW"):
     if (NDIMS==2):
@@ -160,17 +160,24 @@ LOAD_DNS        = True
 INIT_SCA        = 5.0   # N=256=>5, N=512=>10, N=1024=>15
 NC_NOISE_IN     = 1000
 NC2_NOISE_IN    = int(NC_NOISE_IN/2)
-GAUSSIAN_FILTER = True
+EXTERNAL_FILTER = True
+DALPHA          = 0.1
 
-FILE_DNS_N256    = "../../../data/BOUT_runs/HW_2D/Papers/PoP23/HW_N256/fields/fields_HW_run0_time501.npz"
-FILE_DNS_N512    = "../../../data/BOUT_runs/HW_3D/HW_N512x16x512_perX/fields_npz/fields_HW_run0_time298.npz"
+# Create 2D wavenumber grid
+KX  = tf.constant(np.fft.fftfreq(N_DNS, d=LEN_DOMAIN/N_DNS) * 2 * np.pi, dtype=DTYPE)
+KX2 = tf.reshape(KX**2, [N_DNS, 1])  # shape [N_DNS, 1]
+KY2 = tf.reshape(KX**2, [1, N_DNS])  # shape [1, N_DNS]
+
+
+FILE_DNS_N256    = "../../../data/BOUT_runs/HW_2D/Papers/PoP23/HW_N256/fields/fields_HW_run0_time501.npz"  #(DNS is 501)
+FILE_DNS_N512    = "../../../data/BOUT_runs/HW_2D/Papers/PoP23/HW_N512/fields/fields_HW_run0_time301.npz"
 FILE_DNS_N1024   = "../../../data/BOUT_runs/HW_2D/Papers/PoP23/HW_N1024/fields/fields_HW_run0_time440.npz"
 FILE_DNS_N256_3D = "../../../data/BOUT_runs/HW_3D/HW_larger/HW_N256/fields_run6_time400.npz"
 FILE_DNS_N512_3D = "../../../data/BOUT_runs/HW_3D/HW_larger/HW_N256/fields_run1_time300.npz"
 FILE_DNS_N1024_3D = "../../../data/BOUT_runs/HW_3D/HW_Biskamp/fields_npz_3D_1img/fields_run0_time1001.npz"
 
 # learning rate for latent space optimizer
-lr_DNS_maxIt  = 100000
+lr_DNS_maxIt  = 1000000
 lr_DNS_POLICY = "EXPONENTIAL"   # "EXPONENTIAL" or "PIECEWISE"
 lr_DNS_STAIR  = False
 lr_DNS        = 1.0e-3   # exponential policy initial learning rate
@@ -181,4 +188,3 @@ lr_DNS_BOUNDS = [100, 200, 300]             # piecewise policy bounds
 lr_DNS_VALUES = [100.0, 50.0, 20.0, 10.0]   # piecewise policy values
 lr_DNS_BETA1  = 0.0
 lr_DNS_BETA2  = 0.99
-